346b21fec80095721863aacb37e0a7a094222cb4
Jeet Patel <jeet@Jeets-MacBook-Pro.local>
2022-10-28 11:47:30 -0400

    Task tracker app & mock json-server

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
+function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const taskFromServer = await fetchTasks()
+      setTasks(taskFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
+
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+    console.log(id)
+  }
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //Toggle Reminder 
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id
+      ? { ...task, reminder: !task.reminder } : task))
+  }
+
+  return (
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete=
+          {deleteTask} onToggle={toggleReminder} />
+      ) : (
+        'No Task To Show'
+      )}
+    </div>
+  );
+}
+
+export default App;
